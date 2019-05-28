331c1b063e872cff70d04a380747de969ad6a8e1
Ram Singh <ram.c.singh@capgemini.com>
2019-05-28 12:35:02 +0530

    Cleanup

--- webpack/paths.js
+++ webpack/paths.js
@@ -6,7 +6,7 @@ const path = require('path');
 module.exports = {
   root: path.resolve(__dirname, '../'),
   outputPath: path.resolve(__dirname, '../', 'build'),
-  entryPath: path.resolve(__dirname, '../', 'src/Bootstrap.js'),
+  entryPath: path.resolve(__dirname, '../', 'src/bootstrap.js'),
   templatePath: path.resolve(__dirname, '../', 'src/index.html'),
   imagesFolder: 'images',
   fontsFolder: 'fonts',
